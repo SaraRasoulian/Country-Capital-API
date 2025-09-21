aa06e220bdf12cbc4f0addc87c041b379b1b5110
sara rasoulian <sara.rasoulian.dev@gmail.com>
2025-09-21 19:00:00 +0330

    Add const data

---
+++ src/Application.Contract/Countries/SoapResponse/CountrySoapConsts.cs
@@ -0,0 +1,22 @@
+﻿using Application.Contract.Shared;
+
+namespace Application.Contract.Countries.SoapResponse;
+
+public static class CountrySoapConsts
+{
+    public const string Endpoint = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso";
+    public const string Action = "http://www.oorsprong.org/websamples.countryinfo/CountryInfoService.wso/CapitalCity";
+    public const string CountryInfoNamespace = "http://www.oorsprong.org/websamples.countryinfo";
+    public const string Input = "Input";
+    public const string RequestBody = $@"
+    <soap:Envelope 
+        xmlns:soap=""{SharedSoapConsts.EnvelopeNamespace}"" 
+        xmlns:web=""{CountryInfoNamespace}"">
+      <soap:Header/>
+      <soap:Body>
+        <web:CapitalCity>
+          <web:sCountryISOCode>{Input}</web:sCountryISOCode>
+        </web:CapitalCity>
+      </soap:Body>
+    </soap:Envelope>";
+}
---
+++ src/Application.Contract/Shared/SharedSoapConsts.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Contract.Shared;
+
+public static class SharedSoapConsts
+{
+    public const string EnvelopeNamespace = "http://schemas.xmlsoap.org/soap/envelope/";
+}
