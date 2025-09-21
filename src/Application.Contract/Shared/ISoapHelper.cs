62f6372bc956637c0346fafa1f16ec9fa20b7801
sara rasoulian <sara.rasoulian.dev@gmail.com>
2025-09-21 18:59:28 +0330

    Implement Soap Helper

---
+++ src/Application.Contract/Shared/ISoapHelper.cs
@@ -0,0 +1,8 @@
+﻿using Application.Contract.Countries.SoapResponse;
+
+namespace Application.Contract.Shared;
+
+public interface ISoapHelper
+{
+    Task<String> SendRequestAsync(string endpoint, string action, string requestBody);
+}
---
+++ src/Application/Shared/SoapHelper.cs
@@ -0,0 +1,42 @@
+﻿using Application.Contract.Shared;
+using System.Text;
+
+namespace Application.Shared;
+
+public class SoapHelper : ISoapHelper
+{
+    public async Task<String> SendRequestAsync(string endpoint, string action, string requestBody)
+    {
+        try
+        {
+            var content = string.Empty;
+
+            using (HttpClient client = new HttpClient())
+            {
+                var request = new HttpRequestMessage(HttpMethod.Post, endpoint)
+                {
+                    Content = new StringContent(requestBody, Encoding.UTF8, "text/xml")
+                };
+
+                request.Headers.Add("SOAPAction", action);
+
+                HttpResponseMessage response = await client.SendAsync(request);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Error: " + response.StatusCode);
+                    throw new Exception("Soap API Failed.");
+
+                }
+                content = await response.Content.ReadAsStringAsync();
+            }
+
+            return content;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+            throw new Exception("Soap API Failed. " + ex.Message);
+        }
+    }
+}
