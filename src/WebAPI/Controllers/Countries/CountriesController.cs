4651dde6bed83f041931b7949329c7845be0e249
sara rasoulian <sara.rasoulian.dev@gmail.com>
2025-09-21 19:02:10 +0330

    Use country application in country controller

---
+++ src/WebAPI/Controllers/Countries/CountriesController.cs
@@ -0,0 +1,17 @@
+﻿using Application.Contract.Countries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.Countries;
+
+[Route("api/v1/countries")]
+[ApiController]
+public class CountriesController(ICountryApplication countryApplication) : ControllerBase
+{
+    [HttpGet("{isoCode:alpha:length(2)}/capital")]
+
+    public async Task<ActionResult<CountryDto>> GetCapitalAsync(string isoCode)
+    {
+        var result = await countryApplication.GetCapitalAsync(isoCode);
+        return Ok(result);
+    }
+}
